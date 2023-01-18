f89e87768cb3403b93923a42b0a3736ea36c7392
Oppj4 <geed704@gmail.com>
2023-01-18 09:13:10 +0300

    Transferring files to a folder FirstHomeWork

---
+++ FirstHomeWork/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1: 
+// Напишите программу, которая на вход принимает
+// два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2; b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+
+if (numA >= numB){
+    max = numA;
+    min = numB;
+}
+else {
+    max = numB;
+    min = numA;
+}
+
+Console.WriteLine($"Из двух чисел большее {max}, а число {min} - меньшее.");
+
