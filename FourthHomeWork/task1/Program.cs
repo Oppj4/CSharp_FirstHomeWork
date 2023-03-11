925c0b50de78e0b3b19eddaa9148166b7859a389
Oppj4 <geed704@gmail.com>
2023-03-11 19:39:33 +0300

    Fourth Homework task1

---
+++ FourthHomeWork/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1.
+// Напишите программу, которая принимает
+// на вход число (А) и выдаёт сумму чисел
+// от 1 до (A)
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+double sum (long n)
+{
+    if(n == 1) return 1;
+    else return n + sum(n-1);
+}
+
+Console.WriteLine(sum(num));
