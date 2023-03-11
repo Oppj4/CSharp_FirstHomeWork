4f8c931fd1393f3fdfd7a6be54474a0aa840b611
Oppj4 <geed704@gmail.com>
2023-03-11 22:00:25 +0300

    Fourth Homework task2

---
+++ FourthHomeWork/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2.
+// Напишите цикл, который принимает 
+// на вход два числа, (А и В) и возводит
+// число А в натуральную степень В
+// 3, 5 -> 243
+// 2, 5 -> 16
+
+Console.WriteLine("Введите число А ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число В: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int exponentiation(int A, int B)
+{   
+    
+    int result = 1;
+    for(int i = 0; i < B; i++)
+    {
+        result *= A;
+    }
+    return result;
+}
+
+Console.WriteLine(exponentiation(numA, numB));
