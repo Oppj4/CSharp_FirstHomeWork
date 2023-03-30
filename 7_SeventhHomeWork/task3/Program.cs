aa56dc807b054bab385efe39e23cbbcf1c0b8f90
OPPJ4 <geed704@gmail.com>
2023-03-30 21:31:01 +0300

    Seventh homework task 3

---
+++ 7_SeventhHomeWork/task3/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateRndIntMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for(int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void FindElementIndex(int[,] matr)
+{
+    for(int j = 0; j < matr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+           sum += matr[i, j];
+        }
+        double result = sum / matr.GetLength(1);
+        Console.Write($"{result} ");
+    }
+}
+
+
+void printMartix(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if(j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j], 4},");
+            else Console.Write($"{matr[i, j], 4}");
+        }    
+        Console.WriteLine("]");
+    }
+}
+
+int[,] matrix = CreateRndIntMatrix(3, 4, 1, 9);
+printMartix(matrix);
+Console.WriteLine();
+FindElementIndex(matrix);
